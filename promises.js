8851fe001af0a8ea00daf91e2ea37f7c61c5963d
Oladejo <wale@senyours.io>
2019-04-16 14:21:23 -0700

    Promise all

--- promises.js
+++ promises.js
@@ -35,7 +35,17 @@ function createPost(post) {
 //   .then(getPosts);
 //   .catch(err=>console.log(err));
 
+const promise1 = Promise.resolve('hello worls');
+const promise2 = 10;
+const promise3 = new Promise((resolve,reject)=>{
+  setTimeout(resolve,2000,'Goodbye');
+});
+const Promise4 =fetch('https://jsonplaceholder.typicode.com/posts').then(
+  const y = res => res.json()
+  return y
+  );
 
+Promise.all([promise1,promise2,promise3, Promise4]).then((values)=>{console.log(values)});
 
 
 
